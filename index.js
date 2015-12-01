329f5c812ef79a0c442fbecafdd9e8f8aca7cd59
Erick Fleming <erick@sudogs.com>
2015-12-01 11:23:08 -0600

    add logging for tests

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ exports.print = function( contexts ) {
   contexts.forEach( function( context ) {
     console.log( "#", context.name )
     context.tests.forEach( function( test ) {
+      test.logItems.forEach( function( item ) {
+        console.log( "  >",  item )
+      } )
       if ( test.error !== "" ) {
         console.log( "*", test.name )
         console.log( "  ^", test.error )
--- tests.js
+++ tests.js
@@ -6,15 +6,15 @@ var report = [
   {
     name: "context 1",
     tests: [
-      { name: "test 1a", asserts: [ "", "fail" ], error: "" },
-      { name: "test 1b", asserts: [ "", "fail" ], error: "" }
+      { name: "test 1a", asserts: [ "", "fail" ], logItems: [ "thing" ], error: "" },
+      { name: "test 1b", asserts: [ "", "fail" ], logItems: [], error: "" }
     ]
   },
   {
     name: "context 2",
     tests: [
-      { name: "test 2a", asserts: [ "fail", "fail" ], error: "" },
-      { name: "test 2b", asserts: [ "", "fail" ], error: "" }
+      { name: "test 2a", asserts: [ "fail", "fail" ], logItems: [ {name:"foo"} ], error: "" },
+      { name: "test 2b", asserts: [ "", "fail" ], logItems: [{name:"bar"},{other:"gogo"}], error: "" }
     ]
   }
 ]
